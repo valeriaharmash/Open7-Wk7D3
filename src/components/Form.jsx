bc2e54dbe333b4dbb7d8d57a7685477500588a23
Valeriia Harmash <valeriaharmash@gmail.com>
2023-08-21 15:20:39 -0400

    forms demo

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,35 +1,50 @@
-import { useState } from "react";
+import { useState } from 'react'
 
 function Form({ data, setData }) {
-  // controlled inputs use state
-  const [title, setTitle] = useState("");
-  const [description, setDescription] = useState("");
-  const [time, setTime] = useState("");
+	// controlled inputs use state
+	const [title, setTitle] = useState('')
+	const [description, setDescription] = useState('')
+	const [time, setTime] = useState('')
 
-  function submitHandler(e) {
-    // prevent default behavior
-    // create a new todo object that fits the data schema
-    // set the data array to include the new todo object 
-    // clear the form
-  }
+	function submitHandler(e) {
+		// prevent default behavior
+		e.preventDefault()
+		// create a new todo object that fits the data schema
+		const newTodo = {
+			title,
+			description,
+			time
+		}
+		// set the data array to include the new todo object
+		setData([...data, newTodo])
+		setTitle('')
+		setDescription('')
+		setTime('')
+	}
 
-  return (
-    <form onSubmit={submitHandler}>
-      <input
-        type="text"
-        placeholder="Title"
-      />
-      <input
-        type="text"
-        placeholder="Description"
-      />
-      <input
-        type="text"
-        placeholder="Time"
-      />
-      <button type="submit">Submit</button>
-    </form>
-  );
+	return (
+		<form onSubmit={submitHandler}>
+			<input
+				type='text'
+				placeholder='Title'
+				value={title}
+				onChange={(e) => setTitle(e.target.value)}
+			/>
+			<input
+				type='text'
+				placeholder='Description'
+				value={description}
+				onChange={(e) => setDescription(e.target.value)}
+			/>
+			<input
+				type='text'
+				placeholder='Time'
+				value={time}
+				onChange={(e) => setTime(e.target.value)}
+			/>
+			<button type='submit'>Submit</button>
+		</form>
+	)
 }
 
-export default Form;
+export default Form
